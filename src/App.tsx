50a53b64ce5e366178a51d8f18c85f46ba1a8ac2
Nicolás Enciso <enectrl@gmail.com>
2025-04-03 00:16:51 -0300

    feat: add react vite

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+/**
+ * Main application component
+ * @module App
+ * @description This component serves as the entry point for the application.
+ * It imports the necessary styles and renders the main content of the app.
+ * @author Nicolas Enciso for FITX
+ * @date 2025-04-02
+ * @version 1.0.0
+ * @returns {JSX.Element}
+ */
+const App = () => {
+
+  return (
+    <div>FITX</div>
+  )
+}
+
+export default App
